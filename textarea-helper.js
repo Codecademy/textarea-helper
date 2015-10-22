05003f051fc80d978a4f634d1642ebd8f6410026
jjoos <jan@deelstra.org>
2015-10-22 13:41:24 +0200

    Add CommonJs compatability

--- textarea-helper.js
+++ textarea-helper.js
@@ -1,6 +1,17 @@
-(function ($) {
+(function (factory) {
   'use strict';
-
+  if (typeof define === 'function' && define.amd) {
+      // AMD. Register as an anonymous module.
+      define(['jquery'], factory);
+  } else if (typeof module === 'object' && module.exports) {
+      // Node/CommonJS
+      module.exports = factory(require('jquery'));
+  } else {
+      // Browser globals
+      factory(jQuery);
+  }
+}(function ($) {
+  'use strict';
   var caretClass   = 'textarea-helper-caret'
     , dataKey      = 'textarea-helper'
 
@@ -118,4 +129,4 @@
     }
   };
 
-})(jQuery);
+}));
